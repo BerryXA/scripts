9442aded22e4aa149491e9c696b061151e8208d8
BerryXA <119078710+BerryXA@users.noreply.github.com>
2022-11-25 23:47:57 +0300

    Update BerryX.lua

--- BerryX.lua
+++ BerryX.lua
@@ -1,1 +1,13 @@
-print("kso")
+local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local window = ui.CreateLib("Name","RJTheme8")
+
+local tab = window:NewTab("halo")
+
+local selection = tab:NewSelection("selection name")
+
+selection:NewButton("ButtonText","ButtonInfo",function()
+
+    print("swed")
+
+    end)
