74525c3b8c1487f66cf6bad311517ae3ac70018c
BerryXA <119078710+BerryXA@users.noreply.github.com>
2022-11-26 13:25:01 +0300

    Update BerryX.lua

--- BerryX.lua
+++ BerryX.lua
@@ -1,10 +1,10 @@
 local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local window = ui.CreateLib("Name","RJTheme8")
+local window = ui.CreateLib("Test hack","Ocean")
 
 local tab = window:NewTab("hola seniora")
 
-local selection = tab:NewSelection("selection name")
+local selection = tab:NewSection("selection name")
 
 selection:NewToggle("ButtonText","ButtonInfo",function(test)
 if test then
